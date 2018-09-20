3fde3d3eeac3249a1cff846edfae1a6bf50f36dc
zhangyao <zhangyao@chudaokeji.com>
2018-09-20 09:10:23 +0800

    s

--- src/main/java/cn/web/action/user/LoginMgtController.java
+++ src/main/java/cn/web/action/user/LoginMgtController.java
@@ -1,64 +1,48 @@
 package cn.web.action.user;
 
 
-import java.util.Date;
+//import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.baiyue.entity.Banner;
-import com.baiyue.services.IBannerService;
-import com.utils_max.ParseUtils;
+//import com.baiyue.services.IBannerService;
+import com.baiyue.services.cts.user.ICtsUserService;
+import com.baiyue.web.BaseWebAction;
+import com.utils_max.JsonUtil;
+//import com.utils_max.ParseUtils;
+import com.utils_max.ResultMsg;
+import com.utils_max.enums.ResultStatusEnums;
 
 /*
  * 前端用户登陆模块
  */
 @Controller
 @RequestMapping(value = "/sso/")
-public class LoginMgtController {
+public class LoginMgtController  extends BaseWebAction{
 
 	
 	/**
 	 * 用户模块 service api 服务
 //	 */
+//	@Autowired
+//	private IBannerService bannerService;
 	@Autowired
-	private IBannerService bannerService;
-	
+	private ICtsUserService ctsUserService;
 	
 	@ResponseBody
 	@RequestMapping(value = "login")
-	public String download(String id,String pwd) throws Exception {
-		if(bannerService!=null){
-			
-			Banner info=bannerService.selectByPrimaryKey(ParseUtils.parseInt(id));
-			if(info!=null){
-				System.out.println("ok");
-			}else{
-				info=new Banner();
-				info.setAdName("test001"+id);
-				info.setCreateTime(new Date());
-				info.setUpdateTime(new Date());
-				info.setUpdateUserId(12);
-				info.setPublishTime(new Date());
-				info.setStatus(1);
-				info.setUrl("http://baidu.com");
-				info.setNum(2);
-				info.setIsDelete(0); 
-				info.setImgUrl("http://baidu.com");
-				info.setAdName("ceshi312");
-				info.setSliderTime(3213);
-				info.setCreateUserId(1321);
-				int ref=bannerService.insert(info);
-				System.out.println("error!insert result:"+ref); 
-			}
+	public String download(String username,String pwd) throws Exception {
+		ResultMsg result=null;
+		if(ctsUserService!=null){
+			 result=ctsUserService.login(username, pwd);
 		}else{
-			System.out.println(31);
+			result=new ResultMsg();
+			result.setStatus(ResultStatusEnums.error);
+			result.setMsg("LoginMgtController:ctsUserService为null!"); 
 		}
-		
-		System.out.println(id);
-		return  id;
+		return  JsonUtil.objectToJsonStr(result);
 	}
 }
